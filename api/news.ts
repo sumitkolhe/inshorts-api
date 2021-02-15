de2dfe4400994a86d35c5ba89846e5f63eb14b4b
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-02-15 12:49:12 +0530

    :bug: Fixed response delay

--- api/news.ts
+++ api/news.ts
@@ -26,28 +26,31 @@ module.exports = async (req: NowRequest, res: NowResponse) => {
       status: "online",
       Documentation: "https://github.com/sumitkolhe/inshorts-api-v2",
     });
-  }
-  const newsResponse = new Object();
-  const articlesResponse = new Array();
+  } else {
+    const newsResponse = new Object();
+    const articlesResponse = new Array();
 
-  await axios
-    .get(getNewsEndpoint(news_type, Number(limit)))
-    .then((response) => {
-      newsResponse["total"] = Object.keys(response.data.data.news_list).length;
-
-      response.data.data.news_list.forEach((article: Article) => {
-        articlesResponse.push({
-          title: article.news_obj.title,
-          description: article.news_obj.content,
-          author_name: article.news_obj.author_name,
-          source_name: article.news_obj.source_name,
-          source_url: article.news_obj.source_url,
-          image_url: article.news_obj.image_url,
-          created_at: article.news_obj.created_at,
-          inshorts_url: article.news_obj.shortened_url,
-        });
+    let response: any = await axios
+      .get(getNewsEndpoint(news_type, Number(limit)))
+      .catch((err) => {
+        res.json(err);
       });
-      newsResponse["articles"] = articlesResponse;
-      res.json(newsResponse);
+
+    newsResponse["total"] = Object.keys(response.data.data.news_list).length;
+
+    response.data.data.news_list.forEach((article: Article) => {
+      articlesResponse.push({
+        title: article.news_obj.title,
+        description: article.news_obj.content,
+        author_name: article.news_obj.author_name,
+        source_name: article.news_obj.source_name,
+        source_url: article.news_obj.source_url,
+        image_url: article.news_obj.image_url,
+        created_at: article.news_obj.created_at,
+        inshorts_url: article.news_obj.shortened_url,
+      });
     });
+    newsResponse["articles"] = articlesResponse;
+    res.json(newsResponse);
+  }
 };
